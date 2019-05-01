66752367fa5fac2c24137a36ff21b1c791bfdc61
Steven Toth <stoth@kernellabs.com>
2019-05-01 14:05:47 -0400

    make-rpm cleanups

--- rpm/make-rpm.sh
+++ rpm/make-rpm.sh
@@ -17,13 +17,15 @@ GIT_VERSION=`git describe --abbrev=8 | sed 's!-.*!!g'`
 
 cat $SPECFILE  | sed "s/^Version.*$/Version:\t${GIT_VERSION}/g" > ~/rpmbuild/SPECS/$SPECFILE
 
-mkdir -p ~/rpmbuild/BUILDROOT/$APP-$GIT_VERSION-1.x86_64/usr/local/bin
-cp ../src/tstools_util ~/rpmbuild/BUILDROOT/$APP-$GIT_VERSION-1.x86_64/usr/local/bin
+TARGET_DIR=~/rpmbuild/BUILDROOT/$APP-$GIT_VERSION-1.x86_64
 
-mkdir -p ~/rpmbuild/BUILDROOT/$APP-$GIT_VERSION-1.x86_64/usr/local/share/man/man8
-cp ../man/*.8 ~/rpmbuild/BUILDROOT/$APP-$GIT_VERSION-1.x86_64/usr/local/share/man/man8
+mkdir -p $TARGET_DIR/usr/local/bin
+cp ../src/tstools_util $TARGET_DIR/usr/local/bin
 
-pushd ~/rpmbuild/BUILDROOT/$APP-$GIT_VERSION-1.x86_64/usr/local/bin
+mkdir -p $TARGET_DIR/usr/local/share/man/man8
+cp ../man/*.8 $TARGET_DIR/usr/local/share/man/man8
+
+pushd $TARGET_DIR/usr/local/bin
 	for BIN in `./tstools_util | grep ^tstools`
 	do
 		ln -sf tstools_util $BIN
@@ -32,7 +34,7 @@ popd
 
 rpmbuild -bb ~/rpmbuild/SPECS/$SPECFILE
 
-mv /home/stoth/rpmbuild/RPMS/x86_64/$APP-$GIT_VERSION-1.x86_64.rpm .
+mv ~/rpmbuild/RPMS/x86_64/$APP-$GIT_VERSION-1.x86_64.rpm .
 
 # Test the RPM install on a clean centos system.
 # We have a dep on libpcap, ensure yum finds the dep and installs it automatically for us.
