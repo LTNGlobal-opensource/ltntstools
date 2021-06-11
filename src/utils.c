3c8cce37faeb8def63142343ffc8d473e6af261f
Steven Toth <steven.toth@ltnglobal.com>
2021-06-11 12:15:20 -0400

    utils: two additional functions for checking NIC interface names

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 
 /* In string 'str', find occurences of character src and replace with character dst. */
 /* return the number of substituions occured. */
@@ -16,3 +21,67 @@ int character_replace(char *str, char src, char dst)
 
 	return c;
 }
+
+int networkInterfaceExists(const char *ifname)
+{
+	int exists = 0;
+
+	/* Setup multicast on all IPV4 network interfaces, IPV6 interfaces are ignored */
+	struct ifaddrs *addrs;
+	int result = getifaddrs(&addrs);
+	if (result >= 0) {
+		const struct ifaddrs *cursor = addrs;
+		while (cursor != NULL) {
+			if ((cursor->ifa_flags & IFF_BROADCAST) && (cursor->ifa_flags & IFF_UP) &&
+				(cursor->ifa_addr->sa_family == AF_INET)) {
+
+				char host[NI_MAXHOST];
+
+				int r = getnameinfo(cursor->ifa_addr,
+					cursor->ifa_addr->sa_family == AF_INET ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6),
+					host, NI_MAXHOST,
+					NULL, 0, NI_NUMERICHOST);
+				if (r == 0) {
+					if (strcmp(cursor->ifa_name, ifname) == 0) {
+						exists = 1;
+						break;
+					}
+				}
+			}
+			cursor = cursor->ifa_next;
+		}
+	}
+
+	freeifaddrs(addrs);
+
+	return exists;
+}
+
+void networkInterfaceList()
+{
+	/* Setup multicast on all IPV4 network interfaces, IPV6 interfaces are ignored */
+	struct ifaddrs *addrs;
+	int result = getifaddrs(&addrs);
+	if (result >= 0) {
+		const struct ifaddrs *cursor = addrs;
+		while (cursor != NULL) {
+			if ((cursor->ifa_flags & IFF_BROADCAST) && (cursor->ifa_flags & IFF_UP) &&
+				(cursor->ifa_addr->sa_family == AF_INET)) {
+
+				char host[NI_MAXHOST];
+
+				int r = getnameinfo(cursor->ifa_addr,
+					cursor->ifa_addr->sa_family == AF_INET ? sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6),
+					host, NI_MAXHOST,
+					NULL, 0, NI_NUMERICHOST);
+				if (r == 0) {
+					printf("\t%s : %s\n", cursor->ifa_name, host);
+				}
+			}
+			cursor = cursor->ifa_next;
+		}
+	}
+
+	freeifaddrs(addrs);
+}
+
--- src/utils.h
+++ src/utils.h
@@ -3,5 +3,7 @@
 #define LTNTOOLS_UTILS_H
 
 int character_replace(char *str, char src, char dst);
+void networkInterfaceList();
+int  networkInterfaceExists(const char *ifname);
 
 #endif  /* LTNTOOLS_UTILS_H */
