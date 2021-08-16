7c657549e2594441462c2cd183a5445a29fd0d0f
Steven Toth <steven.toth@ltnglobal.com>
2021-08-16 14:56:21 -0400

    core: added some infrastructure to store object hashes and quickly look them up

---
+++ src/hash_index.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "hash_index.h"
+
+struct hash_index_s *hash_index_alloc()
+{
+	struct hash_index_s *p = calloc(65535, sizeof(*p));
+	return p;
+}
+
+void hash_index_free(struct hash_index_s *p)
+{
+	free(p);
+}
+
+void hash_index_set(struct hash_index_s *p, uint16_t key, void *item)
+{
+	struct hash_index_s *e = p + key;
+	e->arr = realloc(e->arr, e->arrLength + 1);
+	//*(e->arr + (e->arrLength * sizeof(struct hash_index_s *))) = (void *)item;
+	*(e->arr + e->arrLength) = (void *)item;
+
+	e->arrLength++;
+}
+
+void *hash_index_get_first(struct hash_index_s *p, uint16_t key)
+{
+	struct hash_index_s *e = p + key;
+	if (e->arrLength == 0)
+		return NULL;
+
+	return e->arr[0];
+}
+
+int hash_index_get_count(struct hash_index_s *p, uint16_t key)
+{
+	struct hash_index_s *e = p + key;
+	return e->arrLength;
+}
+
+int hash_index_get_enum(struct hash_index_s *p, uint16_t key, int *enumer, void **ptr)
+{
+	struct hash_index_s *e = p + key;
+	if (e->arrLength == 0)
+		return -1;
+	if (*enumer < 0 || *enumer >= e->arrLength)
+		return -1;
+	*ptr = e->arr[(*enumer)++];
+
+	return 0;
+}
+
+void hash_index_print(struct hash_index_s *p, uint16_t key)
+{
+	struct hash_index_s *e = p + key;
+	printf("Hash %04x index %p, length %d\n", key, e, hash_index_get_count(p, key));
+	int enumer = 0;
+	int ret = 0;
+	while (ret == 0) {
+		void *ptr = NULL;
+		ret = hash_index_get_enum(p, key, &enumer, &ptr);
+		printf("\tret %d, ptr %p\n", ret, ptr);
+	}
+}
---
+++ src/hash_index.h
@@ -0,0 +1,43 @@
+#ifndef HASH_INDEX_H
+#define HASH_INDEX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+
+/* Inputs are expected to be address in MSB */
+extern uint16_t hash_index_cal_hash(uint32_t addr, uint16_t port);
+
+/* We need hashing mechansim that lets us quickly lookup
+ * entries in the context list of discovered_items,
+ * which is significantly faster when we have 100+
+ * streams to monitor.
+ *
+ *
+ * We take a destination ip address (unicast or multicast)
+ * and its port, the hash transformation looksing like this,
+ * Assuming each character is 4 bits.
+ *
+ * AB.CD.EF.GH:IJKL
+ *
+ * Hash: FGHL
+ */
+struct hash_index_s
+{
+	void **arr;
+	unsigned int arrLength;
+};
+
+struct hash_index_s *hash_index_alloc();
+void  hash_index_free(struct hash_index_s *p);
+void  hash_index_set(struct hash_index_s *p, uint16_t key, void *item);
+
+void *hash_index_get_first(struct hash_index_s *p, uint16_t key);
+int   hash_index_get_count(struct hash_index_s *p, uint16_t key);
+
+int   hash_index_get_enum(struct hash_index_s *p, uint16_t key, int *enumer, void **ptr);
+void  hash_index_print(struct hash_index_s *p, uint16_t key);
+
+#endif /* HASH_INDEX_H */
