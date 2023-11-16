cd31de8bed8a6b1e4fedcb6d8a0dbc86a80bebca
Chris Kennedy <chris.kennedy@ltnglobal.com>
2023-11-16 16:56:20 -0500

    add tr101290 metrics to prometheus bridge

--- probe_ui/prometheus.js
+++ probe_ui/prometheus.js
@@ -18,10 +18,36 @@ const metrics = {
     iat1Min: createGauge('kafka_iat1_min', 'Minimum IAT1', ['dst']),
     iat1Max: createGauge('kafka_iat1_max', 'Maximum IAT1', ['dst']),
     iat1Avg: createGauge('kafka_iat1_avg', 'Average IAT1', ['dst']),
-// "tr101290":{"p1":{"tssyncloss":"OK ","syncbyte":"OK ","pat":"OK ","pat2":"OK ","cc":"OK ","pmt":"OK ","pmt2":"OK ","pid":"BAD"},"p2":{"transport":"OK
-//  ","crc":"OK ","pcr":"OK ","pcrrep":"OK ","cat":"OK "},"p3":{}}
+    // "tr101290":{"p1":{"tssyncloss":"OK ","syncbyte":"OK ","pat":"OK ","pat2":"OK ","cc":"OK ","pmt":"OK ","pmt2":"OK ","pid":"BAD"},"p2":{"transport":"OK
+    //  ","crc":"OK ","pcr":"OK ","pcrrep":"OK ","cat":"OK "},"p3":{}}
     // ... Additional metrics definitions ...
 };
+
+// Prometheus Metrics for tr101290 P1 and P2
+const tr101290P1Metrics = {
+    tssyncloss: createGauge('tr101290_p1_tssyncloss', 'TR101290 P1 TS Sync Loss', ['dst']),
+    syncbyte: createGauge('tr101290_p1_syncbyte', 'TR101290 P1 Sync Byte', ['dst']),
+    pat: createGauge('tr101290_p1_pat', 'TR101290 P1 PAT', ['dst']),
+    pat2: createGauge('tr101290_p1_pat2', 'TR101290 P1 PAT2', ['dst']),
+    cc: createGauge('tr101290_p1_cc', 'TR101290 P1 CC', ['dst']),
+    pmt: createGauge('tr101290_p1_pmt', 'TR101290 P1 PMT', ['dst']),
+    pmt2: createGauge('tr101290_p1_pmt2', 'TR101290 P1 PMT2', ['dst']),
+    pid: createGauge('tr101290_p1_pid', 'TR101290 P1 PID', ['dst'])
+};
+
+const tr101290P2Metrics = {
+    transport: createGauge('tr101290_p2_transport', 'TR101290 P2 Transport', ['dst']),
+    crc: createGauge('tr101290_p2_crc', 'TR101290 P2 CRC', ['dst']),
+    pcr: createGauge('tr101290_p2_pcr', 'TR101290 P2 PCR', ['dst']),
+    pcrrep: createGauge('tr101290_p2_pcrrep', 'TR101290 P2 PCR Repetition', ['dst']),
+    cat: createGauge('tr101290_p2_cat', 'TR101290 P2 CAT', ['dst'])
+};
+
+// Utility function to convert TR101290 status to numeric
+function convertToNumeric(status) {
+    console.log('status:', status)
+    return status.substring(0, 2) === 'OK' ? 0 : 1;
+}
 
 const serviceStreamCount = createGauge('kafka_service_stream_count', 'Number of streams per service', ['dst', 'service_index']);
 
@@ -81,6 +107,19 @@ const run = async () => {
                     console.error('Invalid or missing services:', data.services);
                 }
 
+                // Handle tr101290 P1 Metrics
+                if (data.tr101290 && data.tr101290.p1) {
+                    Object.keys(tr101290P1Metrics).forEach(metric => {
+                        tr101290P1Metrics[metric].labels(dst).set(convertToNumeric(data.tr101290.p1[metric]));
+                    });
+                }
+
+                // Handle tr101290 P2 Metrics
+                if (data.tr101290 && data.tr101290.p2) {
+                    Object.keys(tr101290P2Metrics).forEach(metric => {
+                        tr101290P2Metrics[metric].labels(dst).set(convertToNumeric(data.tr101290.p2[metric]));
+                    });
+                }
             } catch (e) {
                 console.error(`Failed to process message: ${message.value.toString()}`, e);
             }
