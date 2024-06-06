c6149e8f559db34e8389cf8441f9bcf443a70cb3
Jan Alexander Steffens (heftig) <jan.steffens@ltnglobal.com>
2024-06-06 17:39:30 +0200

    Use standard inline

--- src/hexdump.h
+++ src/hexdump.h
@@ -3,7 +3,7 @@
 #ifndef HEXDUMP_H
 #define HEXDUMP_H
 
-__inline__ void hexdump(unsigned char *buf, unsigned int len, int bytesPerRow /* Typically 16 */)
+static inline void hexdump(unsigned char *buf, unsigned int len, int bytesPerRow /* Typically 16 */)
 {
         for (unsigned int i = 0; i < len; i++)
                 printf("%02x%s", buf[i], ((i + 1) % bytesPerRow) ? " " : "\n");
