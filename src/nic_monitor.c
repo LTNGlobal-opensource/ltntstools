abf554cde89addf1733fcd0c31c7feaf62428add
Steven Toth <steven.toth@ltnglobal.com>
2025-03-15 16:18:50 -0400

    tstools_nic_monitor: disabled packet order detection

--- src/nic_monitor.c
+++ src/nic_monitor.c
@@ -406,10 +406,14 @@ static void *ui_thread_func(void *p)
 				for (int i = 0; i < MAX_PID; i++) {
 					if (di->stats->pids[i].enabled) {
 						streamCount++;
+#if 0
 						uint64_t oof_count = ltntstools_pid_stats_stream_get_reorder_errors(di->stats);
+#endif
 						if (i == 0) {
 							mvprintw(streamCount + 2, 0, " -> PID Report");
+#if 0
 							mvprintw(streamCount + 3, 0, "    Pkt Order Errors (POE) %" PRIu64, oof_count);
+#endif
 						}
 
 						mvprintw(streamCount + 2, 37, "0x%04x (%4d) %7.2f %'17" PRIu64 " %12" PRIu64 "\n",
