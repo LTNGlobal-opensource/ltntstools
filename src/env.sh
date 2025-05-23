01a8a05d352c4024799d890f553476fa90908be8
Steven Toth <steven.toth@ltnglobal.com>
2025-05-23 16:05:25 -0400

    env script to set pkg config and library path when interactively build shared version of the tool

---
+++ src/env.sh
@@ -0,0 +1,2 @@
+export PKG_CONFIG_PATH=$PWD/../../target-root/usr/lib64/pkgconfig:$PWD/../../target-root/usr/lib/pkgconfig
+export LD_LIBRARY_PATH=$PWD/../../target-root/usr/lib:$PWD/../../target-root/usr/lib64
