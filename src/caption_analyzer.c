ba6b19d94f15a6f8bcaa1794dc0d5b175a50aec9
Steven Toth <steven.toth@ltnglobal.com>
2025-09-08 11:52:27 +0000

    Moved a duplicate func into the core library - for RBSP stripping

--- src/caption_analyzer.c
+++ src/caption_analyzer.c
@@ -17,26 +17,6 @@ static void dumpPids(struct tool_ctx_s *ctx)
 	}
 }
 
-/* TODO: Move this into the libltntstools once we're completely happy with it.
- * See ISO-14496-10:2004 section 7.3.1 NAL unit Syntax.
- */
-static void ltn_nal_h264_strip_emulation_prevention(struct ltn_nal_headers_s *h)
-{
-	int dropped = 0;
-	for (unsigned int i = 1; i < h->lengthBytes; i++) {
-		if (i + 2 < h->lengthBytes &&
-			h->ptr[i + 0] == 0x00 &&
-			h->ptr[i + 1] == 0x00 &&
-			h->ptr[i + 2] == 0x03)
-		{
-				/* Convert 00 00 03 to 00 00 */
-				memcpy((unsigned char *)&h->ptr[i + 2], &h->ptr[i + 3], h->lengthBytes - i - 3);
-				dropped++;
-		}
-	}
-	h->lengthBytes -= dropped;
-}
-
 static int get_row_first(vbi_page *page)
 {
     for (int i = 0; i < page->rows * page->columns; i++) {
--- src/pes_inspector.c
+++ src/pes_inspector.c
@@ -33,25 +33,7 @@ static int testcase_428_dts_exists(uint32_t dts)
 
 	return 0;
 }
-/* TODO: Move this into the libltntstools once we're completely happy with it.
- * See ISO-14496-10:2004 section 7.3.1 NAL unit Syntax.
- */
-static void ltn_nal_h264_strip_emulation_prevention(struct ltn_nal_headers_s *h)
-{
-	int dropped = 0;
-	for (unsigned int i = 1; i < h->lengthBytes; i++) {
-		if (i + 2 < h->lengthBytes &&
-			h->ptr[i + 0] == 0x00 &&
-			h->ptr[i + 1] == 0x00 &&
-			h->ptr[i + 2] == 0x03)
-		{
-				/* Convert 00 00 03 to 00 00 */
-				memcpy((unsigned char *)&h->ptr[i + 2], &h->ptr[i + 3], h->lengthBytes - i - 3);
-				dropped++;
-		}
-	}
-	h->lengthBytes -= dropped;
-}
+
 
 #if H264_IFRAME_THUMBNAILING
 
